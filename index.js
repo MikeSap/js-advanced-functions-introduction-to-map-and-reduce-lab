cc25038cd9fab02f4cb274e5c580070fa2262fcd
mike <mikesapienzaaudio@gmail.com>
2020-11-05 11:58:25 -0600

    commit tree 32f5b6fbe692d8e289d45d2fccf247407d14e213

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function mapToNegativize(src) {
+  return src.map(x => x * -1)
+}
+
+function mapToNoChange(src) {
+  return src.map(x => x)
+}
+
+function mapToDouble(src) {
+  return src.map(x => x * 2)
+}
+
+function mapToSquare(src) {
+  return src.map(x => x ** 2)
+}
+
+function reduceToTotal(src, startingPoint=0) {
+    let total = startingPoint
+    for (let i = 0; i < src.length; i++ ) {
+      total = total + src[i]
+    }
+    return total
+  }
+  
+  function reduceToAllTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (!src[i]) return false
+    }
+    return true
+  }
+  
+  function reduceToAnyTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (src[i]) return true
+    }
+    return false
+  }
